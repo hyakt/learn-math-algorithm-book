df045239b0c9df5483ad4a19c4a6e624620a2a51
hyakt <hyakt0@gmail.com>
2022-02-15 20:34:50 +0900

    update 問題3.5.2

--- src/bin/p352.rs
+++ src/bin/p352.rs
@@ -4,11 +4,13 @@ fn main() {
     let n = 1_000_000;
 
     let mut count = 0;
+    const X: f64 = 6.0;
+    const Y: f64 = 9.0;
 
     for _ in 0..n {
         let mut rng = thread_rng();
-        let x = rng.gen_range(0f64, 6f64);
-        let y = rng.gen_range(0f64, 9f64);
+        let x = rng.gen_range(0f64, X);
+        let y = rng.gen_range(0f64, Y);
 
         let d1 = ((x - 3.0).powf(2.0) + (y - 3.0).powf(2.0)).sqrt();
         if d1 <= 3.0 {
@@ -22,5 +24,8 @@ fn main() {
         }
     }
 
-    println!("{}", count);
+    println!("x, y: {}, {}", X, Y);
+    println!("丸の中の数: {}", count);
+    println!("丸の割合: {}", count as f64 / n as f64);
+    println!("丸の面積: {}", X * Y * (count as f64 / n as f64));
 }
